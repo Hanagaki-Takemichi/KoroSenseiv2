5071ea3ba6301d4336277067bff5964bdd075c8a
Hanagaki-Takemichi <88120160+Hanagaki-Takemichi@users.noreply.github.com>
2021-08-05 08:25:11 +0700

    Add files via upload

---
+++ Koro-Senseiv2.sh
@@ -0,0 +1,295 @@
+#!/usr/bin/bash
+clear
+bi='\33[34;1m' #biru
+ij='\33[32;1m' #ijo
+pr='\33[35;1m' #purple
+cy='\33[36;1m' #cyan
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+
+#Pembukaan
+clear
+sleep 1
+figlet MF GAMERS OFFICIAL | lolcat
+date | lolcat
+sleep 0.1
+echo "\033[32;1m|----------------------------|"
+sleep 0.1
+echo "|Nama    : Mr.Xios17         |"
+sleep 0.1
+echo "|Youtube : MF GAMERS OFFICIAL|"
+sleep 0.1
+echo "|----------------------------|"
+sleep 0.1
+echo "\033[39;1m[\033[32;1m•\033[39;1m]\033[39;1mLoading..."
+sleep 3
+echo
+clear
+#Youtube
+echo $cy"Subscribe dulu ngab!"
+xdg-open https://youtube.com/channel/UCmBHO8a1jEVlYH5NIuAdfZA
+sleep 5
+clear
+#Password
+pw="sad"
+
+while [ "$pwd" != "$pw" ]
+do
+	echo
+	echo $ij"Masukkan Password"
+	echo
+        read -p "Password : " pwd
+done
+#Tools
+clear
+sleep 2
+clear
+echo $ij"[$pu÷$ij]----------------------------|"
+sleep 1
+echo $ij"[$pu÷$ij]Creator : Mr.Xios17         |"
+sleep 1
+echo $ij"[$pu÷$ij]Youtube : MF GAMERS OFFICIAL|"
+sleep 1
+echo $ij"[$pu÷$ij]----------------------------|"
+echo
+echo
+echo
+sleep 1
+echo
+echo "____________________________"
+echo
+echo $ij"[1]Kereta Api"
+sleep 1
+echo "[2]Tombol Termux"
+sleep 1
+echo "[3]Brutal-Wa"
+sleep 1
+echo "[4]Buat apk virus"
+sleep 1
+echo "[5]200 Tools"
+sleep 1
+echo "[6]Brut-Spaming"
+sleep 1
+echo "[7]Crack FB"
+sleep 1
+echo "[8]Script Uchiha"
+sleep 1
+echo "[9]Vbug"
+sleep 1
+echo "[10]OSIF"
+sleep 1
+echo
+echo $ij"[99]Install Bahan"
+sleep 1
+echo "[00]Keluar/Exit"
+echo
+echo "____________________________"
+
+echo ""
+   read -p "Pilih : " tai
+
+if [ $tai = "1" ]
+then
+echo
+echo
+echo
+pkg install sl
+sl
+sleep 1
+sh Koro-Senseiv2.sh
+fi
+
+if [ $tai = "2" ]
+then
+echo
+echo
+echo
+apt install python
+git clone https://github.com/Sxp-ID/Tombol-Termux
+cd Tombol-Termux
+python main.py
+fi
+
+if [ $tai = "3" ]
+then
+echo
+echo
+echo
+apt install python
+git clone https://github.com/Sxp-ID/Brutal-Wa
+cd Brutal-Wa
+bash install.sh
+python wa.py
+fi
+
+if [ $tai = "4" ]
+then
+echo
+echo
+echo
+apt install tqdm
+pkg install tqdm
+pip2 install tqdm
+pip install tqdm
+git clone https://github.com/rzl2018/BCA-X666X
+cd BCA-X666X
+python2 B-VIRUS.py
+fi
+
+if [ $tai = "5" ]
+then
+echo
+echo
+echo
+apt install bash
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+echo
+echo
+echo
+echo "Username: BAJINGAN"
+sleep 0.5
+echo "Password: Gans"
+echo
+echo
+echo "Ku beri 10 detik buat hapalin username dan pass"
+sleep 1
+echo "Dari sekarang!"
+sleep 10
+sh BAJINGAN.sh
+fi
+
+if [ $tai = "6" ]
+then
+echo
+echo
+echo
+apt install python
+git clone https://github.com/Kingtebe/Brut-Spaming
+cd Brut-Spaming
+echo
+echo
+echo
+echo "Password : f84ceaf1add946f6208332fa515a425f "
+sleep 1
+echo "Kuberi waktu 5 detik buat salin password itu"
+sleep 1
+echo "Dari sekarang!"
+sleep 5
+python v2.py
+fi
+
+if [ $tai = "7" ]
+then
+echo
+echo
+echo
+apt install python2
+pkg install python2
+git clone https://github.com/anggaxd/cid
+cd cid
+echo
+echo
+echo "Username: anggaxd"
+sleep 0.5
+echo "Password: c-all"
+echo
+echo
+echo "Ku beri 10 detik buat hapalin username dan pass"
+sleep 1
+echo "Dari sekarang!"
+sleep 10
+python2 crack.py
+fi
+
+if [ $tai = "8" ]
+then
+echo
+echo
+echo
+git clone https://github.com/AbyssExploit/Uchiha
+cd Uchiha
+echo
+echo
+echo "Username: Abyss-Walker"
+sleep 0.5
+echo "Password: UchihaClan"
+echo
+echo
+echo "Ku beri 10 detik buat hapalin username dan pass"
+sleep 1
+echo "Dari sekarang!"
+sleep 10
+bash setup.sh
+bash sharingan.sh
+fi
+
+if [ $tai = "9" ]
+then
+echo
+echo
+echo
+apt install python
+git clone https://github.com/Junior60/vbug
+cd vbug
+python2 vbug.py
+fi
+
+if [ $tai = "10" ]
+then
+echo
+echo
+echo
+apt install
+git clone https://github.com/Ciku370/OSIF
+cd OSIF
+pip install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $tai = "99" ]
+then
+echo
+echo
+echo
+echo "$ij[$pu•$ij]Penginstallan..."
+sleep 3
+echo
+echo
+apt update && apt upgrade
+pkg update && pkg upgrade
+apt install lolcat
+apt install figlet
+apt install python2
+apt install python3
+apt install python
+apt install wget
+apt install ruby
+apt install pip2
+pkg install pip2
+pip2 install requests
+pip2 install mechanize
+pip install bs4
+apt install clang
+echo
+echo
+echo "$ij[$pu•$ij] Complete..."
+echo
+echo "Login..."
+sleep 3
+sh Koro-Senseiv2.sh
+fi
+
+if [ $tai = 00 ]
+then
+sleep 1
+echo
+echo
+echo
+echo "$ij[÷]" $me" Keluar dari perogram"
+sleep 2
+echo
+exit
+
+  fi
